be07746f40b5ffd4de14bef951712d732aaf126e
mirella.alves <mirella.alves@gmail.com>
2021-10-11 16:44:39 -0300

    create data file and start to call them at index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import initialData from './initial-data';
 
-const App = () => 'Hello World'
+const App = () => {
+  const [tasksList, settasksList] = useState(initialData);
+
+  return (
+    tasksList.columnOrder.map((columnId) => {
+      const column = tasksList.columns[columnId];
+      const tasks = column.taskIds.map(taskId => tasksList.tasks[taskId]);
+
+      return column.title;
+    })
+  )
+}
 
 ReactDOM.render(
     <App />,
