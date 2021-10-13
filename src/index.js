86b9d421964fd1e066d60fd0fd25d1421ffd701c
mirella.alves <mirella.alves@gmail.com>
2021-10-13 11:11:38 -0300

    include onDragEnd to move tasks order

--- src/index.js
+++ src/index.js
@@ -7,8 +7,44 @@ import Column from './components/column';
 const App = () => {
 	const [tasksList, settasksList] = useState(initialData);
 
+	const onDragEnd = (result) => {
+		const { destination, source, draggableId } = result;
+		console.log('result:', result)
+
+		if (!destination) {
+			return;
+		}
+
+		if (
+			destination.droppableId === source.droppableId &&
+			destination.index === source.index
+		) {
+			return;
+		}
+
+		const column = tasksList.columns[source.droppableId];
+		const newTaskIds = Array.from(column.taskIds);
+		newTaskIds.splice(source.index, 1);
+		newTaskIds.splice(destination.index, 0, draggableId);
+
+		const newColumn = {
+			...column,
+			taskIds: newTaskIds,
+		}
+
+		const newListState = {
+			...tasksList,
+			columns: {
+				...tasksList.columns,
+				[newColumn.id]: newColumn,
+			},
+		}
+
+		return settasksList(newListState);
+	}
+
 		return (
-			<DragDropContext>
+			<DragDropContext onDragEnd={onDragEnd}>
 				{tasksList.columnOrder.map((columnId) => {
 					const column = tasksList.columns[columnId];
 					const tasks = column.taskIds.map(taskId => tasksList.tasks[taskId]);
